d5d3cd13003226ee96a896a1b99165c9e959221b
synweap15 <shamdog@gmail.com>
2015-03-29 15:33:21 +0200

    explicit error message

--- algorithms/letter_recognition_simple/digit_neuron.py
+++ algorithms/letter_recognition_simple/digit_neuron.py
@@ -35,7 +35,7 @@ class DigitNeuron(object):
                 self._memory = pickle.load(open("tmp/{}.data".format(self._digit), "rb"))
                 self._loaded_from_file = True
             except IOError:
-                print("!")
+                print("Couldn't load tmp/{}!".format(self._digit))
 
     def train(self, data):
         image_array = np.array(data)
